aa61c260227efe8f7a98b7a093a645a22b232e53
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-23 20:30:10 +0300

    UPD HomeScreen 0.0.7

--- lib/feature/main_screen/my_drawer.dart
+++ lib/feature/main_screen/my_drawer.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import '../../core/all_file.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
+    return Drawer(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('data'),
+          Stack(
+            children: [
+              const CircleAvatar(
+                radius: 100,
+                backgroundColor: Colors.white,
+              ),
+              Positioned(
+                top: getScreenSize(context).height / 2 - 400,
+                left: getScreenSize(context).width / 2 - 175,
+                child: const CircleAvatar(
+                  radius: 80,
+                  backgroundImage: NetworkImage(
+                      'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png'),
+                ),
+              ),
+            ],
+          ),
+          MaterialButton(onPressed: () {}, child: const Text('Logout'))
         ],
       ),
     );
