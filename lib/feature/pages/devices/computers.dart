c4d9e43b4c2573142cd2acfbc2d751dc0438aeaa
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-24 08:30:03 +0300

    UPD Computers version 0.0.2

--- lib/feature/pages/devices/computers.dart
+++ lib/feature/pages/devices/computers.dart
@@ -18,6 +18,31 @@ class Computers extends StatelessWidget {
           child: Column(
             children: [
               myContainer(
+                  imagePath: 'assets/images/dev/hp15.6.png',
+                  width: 1.3,
+                  height: 2.79,
+                  fit: null),
+              Padding(
+                padding: EdgeInsets.all(getScreenSize(context).height / 85),
+                child: Card(
+                  child: Container(
+                    width: getScreenSize(context).width / 1.3,
+                    height: getScreenSize(context).height / 9.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: MyColors.colorWhite,
+                    ),
+                    child: const ListTile(
+                      title: Text('HP Laptop 15.6 inch'),
+                      subtitle: Text('Price: 2000 RS'),
+                      leading: Icon(Icons.computer),
+                      trailing: Icon(Icons.add_shopping_cart),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: getScreenSize(context).height / 35),
+              myContainer(
                   imagePath: 'assets/images/dev/hp15s.png',
                   width: 1.3,
                   height: 2.79,
@@ -33,22 +58,14 @@ class Computers extends StatelessWidget {
                       color: MyColors.colorWhite,
                     ),
                     child: const ListTile(
-                      title: Text('hp laptop 15.6 inch'),
-                      subtitle: Text('Price: 2000 RS'),
+                      title: Text('HP Notebook 15s'),
+                      subtitle: Text('Price: 1700 RS'),
                       leading: Icon(Icons.computer),
                       trailing: Icon(Icons.add_shopping_cart),
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: getScreenSize(context).height / 35,
-              ),
-              myContainer(
-                  imagePath: 'assets/images/dev/hp15s.png',
-                  width: 1.3,
-                  height: 2.79,
-                  fit: null),
             ],
           ),
         ),
