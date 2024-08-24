289fef269b4eece685d8ade728caceb04fa7ae60
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-24 09:51:08 +0300

    add shimmer

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/global.dart';
 import 'package:shopping/widget/product_view.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late bool isLoading;
+  @override
+  void initState() {
+    isLoading = true;
+    setState(() {});
+
+    Future.delayed(const Duration(seconds: 5), () {
+      isLoading = false;
+      setState(() {});
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +49,18 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                   Flexible(
-                    child: TabBarView(
-                      children: [
-                        ProductView(products: cloths),
-                        ProductView(products: electronics),
-                        ProductView(products: schools),
-                      ],
-                    ),
+                    child: isLoading
+                        ? Shimmer(
+                            child: const Center(
+                            child: Text("Loading"),
+                          ))
+                        : TabBarView(
+                            children: [
+                              ProductView(products: cloths),
+                              ProductView(products: electronics),
+                              ProductView(products: schools),
+                            ],
+                          ),
                   ),
                 ],
               ),
