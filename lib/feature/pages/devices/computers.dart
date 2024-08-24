990b0e82c1135bd820d1146a10c25fa80a53dcb6
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-24 07:42:11 +0300

    UPD Computers version 0.0.1

--- lib/feature/pages/devices/computers.dart
+++ lib/feature/pages/devices/computers.dart
@@ -19,9 +19,20 @@ class Computers extends StatelessWidget {
                   width: 1.3,
                   height: 2.79,
                   fit: null),
+              SizedBox(height: getScreenSize(context).height / 45),
+              const Text(
+                'ComputersComputersComputersComputersComputersComputersComputersComputersComputersComputers',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
               SizedBox(
                 height: getScreenSize(context).height / 5,
               ),
+              myContainer(
+                  imagePath: 'assets/images/dev/hp15s.png',
+                  width: 1.3,
+                  height: 2.79,
+                  fit: null),
             ],
           ),
         ),
