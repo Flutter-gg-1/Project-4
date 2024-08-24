e367bbbe486280449ac32088e11f5c77ef1bd952
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-24 11:02:52 +0300

    add sliding banners

--- lib/src/special_offer_screen.dart
+++ lib/src/special_offer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 class SpecialOfferScreen extends StatelessWidget {
   const SpecialOfferScreen({super.key});
@@ -6,22 +7,29 @@ class SpecialOfferScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
-           showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Special Offer'),
-          content: const Text('Check our special descount this Black Friday'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
+      onTap: () {
+        showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Special Offer'),
+            content: const Text('Check our special descount this Black Friday'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
       },
-      child: Image.asset("assets/images/offer.jpg"),
+      child: ImageSlideshow(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          children: [
+            Image.asset("assets/images/offer.jpg"),
+            Image.asset("assets/images/sales.jpg"),
+            Image.asset("assets/images/sales 2.jpg"),
+          ]),
     );
   }
 }
